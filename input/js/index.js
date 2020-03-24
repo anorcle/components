a3d336b6ae7e5b7c85d35874076cb463f4799e40
Suraj Jha <contact@jhasuraj.com>
2020-03-24 12:46:42 +0530

    Refractored to es5

--- input/js/index.js
+++ input/js/index.js
@@ -1,17 +1,9 @@
-let all = document.querySelectorAll('.in');
-[...all].forEach(elm => elm.addEventListener('focus', (eve) => {
-    console.log('focused', eve.target);
-}));
-[...all].forEach(elm => elm.addEventListener('blur', (eve) => {
-    console.log('blured', eve.target);
-}))
-
-document.getElementById('input1').addEventListener('focus', (eve) => {
+document.getElementById('input1').addEventListener('focus', function (eve) {
     let label = eve.target.parentElement.children[1];
     label.classList.add('float');
 });
 
-document.getElementById('input1').addEventListener('blur', (eve) => {
+document.getElementById('input1').addEventListener('blur', function (eve) {
     if(event.target.value === '') {
         let label = eve.target.parentElement.children[1];
         label.classList.remove('float');
